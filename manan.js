d5030a74c7ce39404743520d9bbc764075e2d8ea
zelzal-tech <58481150+zelzal-tech@users.noreply.github.com>
2019-12-10 19:43:10 +0200

    Update manan.js

--- manan.js
+++ manan.js
@@ -21,7 +21,7 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);var x=Math.floor((Math.random()*4)+1);var xxx=null;if(x=="1"){xxx="the-way-to-start-business-at-home.html"}
 if(x=="2"){xxx="10-tips-for-start-small-business-startup.html"}
 if(x=="3"){xxx="advantages-of-being-home-business-owner.html"}
-if(x=="4"){xxx="practice-environmental-qwareness-to-our.html"}a_to_ve[i].href="https://links.th3farhat.com/p/urh.html?url="+encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="practice-environmental-qwareness-to-our.html"}a_to_ve[i].href="https://may-links.blogspot.com/p/convert.html?url="+encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
